21e58a86ebea3447a9454073f2766ef74654cf2a
Bijan Fakhri <bfakhri@asu.edu>
2020-04-18 10:20:02 -0700

    U

--- eval.py
+++ eval.py
@@ -15,3 +15,7 @@ hand = [
 ]
 
 print(Card.print_pretty_cards(board + hand))
+
+# Hand strength is valued on a scale of 1 to 7462, where 1 is a Royal Flush and 7462 is unsuited 7-5-4-3-2
+evaluator = Evaluator()
+print(evaluator.evaluate(board, hand))
