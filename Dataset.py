e03c77ed64af68bd3b98f07a2cc8edb8db8c37c8
Bijan Fakhri <bfakhri@asu.edu>
2020-04-19 08:58:52 -0700

    Added randomization

--- Dataset.py
+++ Dataset.py
@@ -1,5 +1,6 @@
 # Defines the dataset
 import cv2
+import numpy as np
 
 class CardDataset:
     def __init__(self):
@@ -7,33 +8,45 @@ class CardDataset:
         self.suits = ['s','h','d','c']
         self.suit_colors = {'s': (1,1,1),'h': (1,1,255),'d': (255,1,1),'c': (2,130,2)}
         self.ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-        self.fonts = [cv2.FONT_HERSHEY_SIMPLEX, 'FONT_HERSHEY_PLAIN', 'FONT_HERSHEY_DUPLEX', 'FONT_HERSHEY_COMPLEX', 'FONT_HERSHEY_TRIPLEX', 'FONT_HERSHEY_COMPLEX_SMALL', 'FONT_HERSHEY_SCRIPT_SIMPLEX', 'FONT_HERSHEY_SCRIPT_COMPLEX']
+        self.fonts = [cv2.FONT_HERSHEY_SIMPLEX, cv2.FONT_HERSHEY_PLAIN, cv2.FONT_HERSHEY_DUPLEX, cv2.FONT_HERSHEY_COMPLEX, cv2.FONT_HERSHEY_TRIPLEX, cv2.FONT_HERSHEY_COMPLEX_SMALL, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, cv2.FONT_HERSHEY_SCRIPT_COMPLEX]
 
         self.img_blank_card = cv2.imread(img_dir+'blank_card.png')
         self.imgs_suit = {}
         for suit in self.suits:
             self.imgs_suit[suit] = cv2.imread(img_dir+'suit_'+suit+'.png')
 
-    def make_card(self, suit, rank):
-        # Params
-        suit_x_offs = 3 
-        suit_y_offs = 3 
-        rank_x_offs = 5 
-        rank_y_offs = 6
-        font = self.fonts[0]
-        font_scale = 0.65
-        #color = (255, 0, 0) 
-        color = self.suit_colors[suit]
-        thickness = 1
-
+    def make_card(self, suit, rank, randomize=True):
         # Get the base images
         img_card = self.img_blank_card.copy()
         img_suit = self.imgs_suit[suit].copy()
+
+        # Params
+        if(randomize):
+            suit_x_offs = np.random.randint(1,4) 
+            suit_y_offs = np.random.randint(1,4) 
+            rank_x_offs = np.random.randint(4,7) 
+            rank_y_offs = np.random.randint(5,8) 
+            font = self.fonts[np.random.randint(0,len(self.fonts)-1)]
+            font_scale = np.random.rand(1)*0.2 + 0.55 
+            thickness = np.random.rand(1)*0.2 + 0.9
+            suit_scale = np.random.rand(2)*0.5 + 0.9
+            img_suit = cv2.resize(img_suit, (0,0), fx=suit_scale[0], fy=suit_scale[1])
+        else:
+            suit_x_offs = 3 
+            suit_y_offs = 3 
+            rank_x_offs = 5 
+            rank_y_offs = 6
+            font = self.fonts[0]
+            font_scale = 0.65
+            thickness = 1
+        color = self.suit_colors[suit]
+
         # Place suit on blank card
         img_card[suit_y_offs:suit_y_offs+img_suit.shape[0], suit_x_offs:suit_x_offs+img_suit.shape[1], :] = img_suit
         img_card[img_card.shape[0]-img_suit.shape[0]-suit_y_offs:img_card.shape[0]-suit_y_offs, img_card.shape[1]-img_suit.shape[1]-suit_x_offs:img_card.shape[1]-suit_x_offs, :] = img_suit
         # Place rank on card
         rank_coords = (int(img_card.shape[1]/2-rank_x_offs), int(img_card.shape[0]/2+rank_y_offs))
+        print(rank, rank_coords, font, font_scale, color, thickness, cv2.LINE_AA)
         img_card = cv2.putText(img_card, rank, rank_coords, font, font_scale, color, thickness, cv2.LINE_AA)
 
 
