d2fe0e7e1357b55c2a8357c0c7cdaa4d450bc19c
Bijan Fakhri <bfakhri@asu.edu>
2020-04-18 22:51:39 -0700

    Used Max Pooling

--- model.py
+++ model.py
@@ -13,47 +13,41 @@ class CardClassifier(tf.keras.Model):
 
         # Params
         num_filters = 52
-        num_layers = 16
+        num_conv_lyrs = 16
 
         # Multi-scale feature extractor layers
         self.fe_lyrs = []
-        for i in range(num_layers):
+        for i in range(num_conv_layers):
             self.fe_lyrs.append(tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'))
 
         # Classification layers
-        self.recon_lyr = tf.keras.layers.Conv2D(filters=3, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')
+        self.class_lyrs = []
+        for i in range(num_conv_layers):
+            self.class_lyrs.append(tf.keras.layers.Dense(units=num_filters, activation='relu'))
 
     def call(self, x, reconstruct=True):
         ''' 
         Trains as an autoencoder but provides features during inference
         '''
         h = x
-        saved_h = []
         print('Orig: ', h.shape)
         # Small scale feature extractor layers
         for idx,layer in enumerate(self.fe_lyrs):
             h = layer(h)
-            if(np.any(idx == self.stops)):
-                saved_h.append(h)
-                print(idx, h.shape, '\tSaved!')
-            else:
-                print(idx, h.shape)
+            print(idx, h.shape)
 
-        if(reconstruct):
-            # Concat all the saved features for reconstruction
-            saved_h_rs = []
-            for feats in saved_h:
-                print('Resizing: ', feats.shape[1:3], ' to: ', x.shape[1:3])
-                saved_h_rs.append(tf.image.resize(feats, x.shape[1:3]))
-                print('new size: ', saved_h_rs[-1].shape)
-            h = tf.concat(saved_h_rs, axis=-1)
-            print('Concated: ', h.shape)
-            # Perform reconstruction
-            h = self.recon_lyr(h)
-            print('Reconstructed: ', h.shape)
-            return h 
-        else:
-            return saved_h
+        # Reduce on spatial dimensions (bs, w, h, c) -> (bs, c)
+        (bs, w, h, c) = h.shape
+        h = tf.nn.max_pool(h, [w, h])
+        for idx,layer in enumerate(self.class_lyrs):
+            h = layer(h)
+            print(idx, h.shape)
+
+        preds = tf.nn.sigmoid(h)
+
+        return preds
+
+        
 
 
         
